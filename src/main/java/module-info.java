1409b97483668bcfcaadd8664accbfe44f0a1324
HardBoss07 <matteo@bosshard.net>
2025-02-21 11:13:41 +0100

    init commit #2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module ch.bosshard.matteo.blockblast {
 
     opens ch.bosshard.matteo.blockblast to javafx.fxml;
     exports ch.bosshard.matteo.blockblast;
+    exports ch.bosshard.matteo.blockblast.blocks;
+    opens ch.bosshard.matteo.blockblast.blocks to javafx.fxml;
 }
